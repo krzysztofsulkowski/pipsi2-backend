f7f52d91c05a23376777bb7ae06a91aa17b824ed
Anna Trybel <annakalinovich717@gmail.com>
2025-10-06 15:14:46 +0200

    Add BudgetApiDbContext class and update project file structure

---
+++ budget-api/Models/BudgetApiDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace budget_api.Models
+{
+    public class BudgetApiDbContext : IdentityDbContext
+    {
+        public BudgetApiDbContext(DbContextOptions<BudgetApiDbContext> options) : base(options) { }
+    }
+}
