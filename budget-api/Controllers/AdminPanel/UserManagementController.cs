27f21979d07a3651d12b5da4fa181cdd2279aad5
Anna Trybel <annakalinovich717@gmail.com>
2025-10-17 17:34:17 +0200

    Implement User Management functionality with CRUD operations and user locking/unlocking

---
+++ budget-api/Controllers/AdminPanel/UserManagementController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using budget_api.Models;
+using budget_api.Models.Dto;
+using budget_api.Services.Results;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using budget_api.Services.Interfaces;
+
+namespace budget_api.Controllers.Admin
+{
+    [ApiController]
+    [Authorize(Roles = "Administrator")]
+    [Route("AdminPanel/Users")]
+    public class UserManagementController : BudgetApiBaseController
+    {
+        private readonly IUserManagementService _userManagementService;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UserManagementController(IUserManagementService userManagementService, RoleManager<IdentityRole> roleManager)
+        {
+            _userManagementService = userManagementService;
+            _roleManager = roleManager;
+        }
+
+        //[HttpGet("Roles")]
+        //[ApiExplorerSettings(IgnoreApi = true)]
+        //public async Task<IActionResult> GetAllRoles()
+        //{
+        //    var roles = await _roleManager.Roles.ToListAsync();
+        //    return HandleServiceResult(roles);
+        //}
+
+        [HttpPost("GetAllUsers")]
+        public async Task<IActionResult> GetAllUsers([FromForm] DataTableRequest request)
+        {
+            var serviceResponse = await _userManagementService.GetAllUsers(request);
+            return HandleStatusCodeServiceResult(serviceResponse);
+        }
+
+        [HttpPost("CreateUser")]
+        public async Task<IActionResult> CreateUser([FromForm] UserDto user)
+        {
+            var result = await _userManagementService.CreateUser(user);
+            return HandleStatusCodeServiceResult(result);
+        }
+
+        [HttpPost("UpdateUser")]
+        public async Task<IActionResult> UpdateUser([FromForm] UserDto user)
+        {
+            var result = await _userManagementService.UpdateUser(user);
+            return HandleServiceResult(result);
+        }
+
+        [HttpPost("LockUser/{userId}")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public async Task<IActionResult> LockUser([FromRoute] string userId)
+        {
+            if (string.IsNullOrEmpty(userId))
+            {
+                return BadRequest(ServiceResult.Failure("Nieprawidłowy identyfikator użytkownika"));
+            }
+
+            string? currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (currentUserId != null && userId == currentUserId)
+            {
+                return BadRequest(ServiceResult.Failure("Nie można zablokować własnego konta"));
+            }
+
+            var result = await _userManagementService.LockUser(userId);
+            return HandleServiceResult(result);
+        }
+
+        [HttpPost("UnlockUser/{userId}")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public async Task<IActionResult> UnlockUser([FromRoute] string userId)
+        {
+            if (string.IsNullOrEmpty(userId))
+            {
+                return BadRequest(ServiceResult.Failure("Nieprawidłowy identyfikator użytkownika"));
+            }
+
+            var result = await _userManagementService.UnlockUser(userId);
+            return HandleServiceResult(result);
+        }
+
+        [HttpGet("GetUserById")]
+        public async Task<IActionResult> GetUserById(string userId)
+        {
+            if (string.IsNullOrEmpty(userId))
+            {
+                return BadRequest(ServiceResult.Failure("Nieprawidłowy parametr: ID użytkownika nie może być puste\r\n"));
+            }
+
+            var result = await _userManagementService.GetUserById(userId);
+            return HandleStatusCodeServiceResult(result);
+        }
+    }
+}
+
+
+
+
---
+++ budget-api/Services/Interfaces/IUserManagementService.cs
@@ -0,0 +1,16 @@
+﻿using budget_api.Models.Dto;
+using budget_api.Models;
+using budget_api.Services.Results;
+
+namespace budget_api.Services.Interfaces
+{
+    public interface IUserManagementService
+    {
+        Task<ServiceResult<DataTableResponse<UserDto>>> GetAllUsers(DataTableRequest request);
+        Task<ServiceResult> LockUser(string userId);
+        Task<ServiceResult> UnlockUser(string userId);
+        Task<ServiceResult<UserDto>> CreateUser(UserDto user);
+        Task<ServiceResult> UpdateUser(UserDto model);
+        Task<ServiceResult<UserDto>> GetUserById(string userId);
+    }
+}
