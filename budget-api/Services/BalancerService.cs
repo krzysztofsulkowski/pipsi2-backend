979afdc63fa02c41b3e7d8ab3f398b12c8f10c0c
Anna Trybel <annakalinovich717@gmail.com>
2025-10-18 18:18:38 +0200

    Add  BalancerService

---
+++ budget-api/Services/BalancerService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace budget_api.Services
+{
+    public class BalancerService
+    {
+        private readonly ILogger<BalancerService> _logger;
+
+        public BalancerService(ILogger<BalancerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<string?> GenerateEmailTemplateAsync(string? senderName, string budgetName, string invitationUrl)
+        {
+            string templateFileName = "SharedBudgetInvitationTemplate.html";
+            string templateDirectory = Path.Combine(AppContext.BaseDirectory, "Templates");
+            string templatePath = Path.Combine(templateDirectory, templateFileName);
+
+            string emailTemplateContent;
+            try
+            {
+                emailTemplateContent = await File.ReadAllTextAsync(templatePath);
+            }
+            catch (DirectoryNotFoundException)
+            {
+                _logger.LogError("Nie znaleziono folderu szablonów email: {TemplateDirectory}", templateDirectory);
+                return null;
+            }
+            catch (FileNotFoundException)
+            {
+                _logger.LogError("Nie znaleziono pliku szablonu email: {TemplatePath}", templatePath);
+                return null;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Błąd podczas odczytu szablonu email z pliku: {TemplatePath}", templatePath);
+                return null;
+            }
+
+            emailTemplateContent = emailTemplateContent.Replace("{InviterName}", senderName ?? "Znajomy");
+            emailTemplateContent = emailTemplateContent.Replace("{BudgetName}", budgetName);
+            emailTemplateContent = emailTemplateContent.Replace("{InvitationUrl}", invitationUrl);
+
+            return emailTemplateContent;
+        }
+    }
+}
