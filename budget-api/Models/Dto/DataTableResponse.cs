8021fc0ef206c5443544f446d6b1dbad8ca387e0
Anna Trybel <annakalinovich717@gmail.com>
2025-10-18 18:13:53 +0200

    Add DataTableRequest and DataTableResponse DTOs

---
+++ budget-api/Models/Dto/DataTableResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace budget_api.Models.Dto
+{
+    public class DataTableResponse<T>
+    {
+        public int Draw { get; set; }
+        public int RecordsTotal { get; set; }
+        public int RecordsFiltered { get; set; }
+        public List<T> Data { get; set; }
+    }
+}
