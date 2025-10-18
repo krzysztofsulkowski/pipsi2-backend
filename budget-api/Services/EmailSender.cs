51d0297b1187a04a2ee465ab55b2b0ba2680c6a7
Anna Trybel <annakalinovich717@gmail.com>
2025-10-18 18:18:53 +0200

    Add EmailSender implementations

---
+++ budget-api/Services/EmailSender.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using System.Net;
+using System.Net.Mail;
+
+namespace budget_api.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public IConfiguration config { get; }
+        public EmailSender(IConfiguration config)
+        {
+            this.config = config;
+        }
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            using (MailMessage mailMessage = new MailMessage(config["EmailConfiguration:From"], email))
+            {
+                mailMessage.Subject = subject;
+                mailMessage.Body = htmlMessage;
+                mailMessage.IsBodyHtml = true;
+
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = config["EmailConfiguration:SmtpServer"];
+                smtp.EnableSsl = bool.Parse(config["EmailConfiguration:EnableSsl"]);
+                NetworkCredential networkCredentials = new NetworkCredential(config["EmailConfiguration:Username"], config["EmailConfiguration:Password"]);
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = networkCredentials;
+                smtp.Port = int.Parse(config["EmailConfiguration:Port"]);
+
+                await smtp.SendMailAsync(mailMessage);
+            }
+        }
+    }
+}
