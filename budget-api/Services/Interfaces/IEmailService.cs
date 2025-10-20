4fca51a3d4d420d263b197c73477c875f2fe5fa6
Anna Trybel <anna.trybel@softiq.pl>
2025-10-20 13:54:35 +0200

    Delete IEmailSender

--- budget-api/Services/Interfaces/IEmailService.cs
+++ budget-api/Services/Interfaces/IEmailService.cs
@@ -1,11 +1,11 @@
 ﻿using budget_api.Models.ViewModel;
+using budget_api.Services.Results;
 
 namespace budget_api.Services.Interfaces
 {
     public interface IEmailService
     {
-        Task SendContactMessageToAdminAsync(ContactMessageViewModel message);
-        Task SendContactConfirmationToUserAsync(ContactMessageViewModel message);
-        Task SendInvitationAsync(string recipientEmail, string senderName, string budgetName, string invitationUrl);
+        Task<ServiceResult> SendContactMessageToAdminAsync(ContactMessageViewModel message);
+        Task<string?> SendInvitationAsync(string? senderName, string budgetName, string invitationUrl);
     }
 }
