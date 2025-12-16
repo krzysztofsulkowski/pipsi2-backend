2802045ca77188f3b04943ab0a0482d26ba5b6e7
Krzysztof Sułkowski <krzysztof.sulkowski@studenci.collegiumwitelona.pl>
2025-12-16 18:39:07 +0100

    Establish new files for create budget API tests

---
+++ tests/BudgetApi.PlaywrightTests/BudgetCoreAPITests.cs
@@ -0,0 +1,25 @@
+﻿/*
+POST /api/budget/create
+
+GET / api / budget /{ budgetId}
+
+GET / api / budget / my - budgets
+
+POST / api / budget /{ budgetId}/ edit
+
+POST / api / budget /{ budgetId}/ archive
+
+POST / api / budget /{ budgetId}/ unarchive
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetApi.PlaywrightTests
+{
+    class BudgetCoreAPITests
+    {
+    }
+}
---
+++ tests/BudgetApi.PlaywrightTests/BudgetMembersAPITests.cs
@@ -0,0 +1,15 @@
+﻿/*
+DELETE /api/budget/{budgetId}/members/{userId}
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetApi.PlaywrightTests
+{
+    class BudgetMembersAPITests
+    {
+    }
+}
